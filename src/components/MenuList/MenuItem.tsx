75f778b1dba4caca7046c2f64084d7eee1e13774
pritulaziah <ziahherzen@gmail.com>
2022-07-25 18:47:28 +0500

    fix menu itm title and weight text bug

--- src/components/MenuList/MenuItem.tsx
+++ src/components/MenuList/MenuItem.tsx
@@ -8,14 +8,14 @@ const MenuItem = ({ item }: IProps) => {
   return (
     <div className="flex flex-col px-5 py-4 bg-white transition-shadow shadow hover:shadow-md">
       <div className="mb-2 flex justify-between text-lg">
-        <div className="flex items-center">
-          <h4>{item.title}</h4>
+        <div className="inline-block">
+          <h4 className="inline">{item.title}</h4>
           {item.weight && (
             <span className="whitespace-nowrap text-sm text-gray-400 ml-2">{`${item.weight} г`}</span>
           )}
         </div>
         {item.price && (
-          <span className="whitespace-nowrap ml-2 flex items-center">{`${item.price} ₽`}</span>
+          <span className="whitespace-nowrap ml-2">{`${item.price} ₽`}</span>
         )}
       </div>
       <div
